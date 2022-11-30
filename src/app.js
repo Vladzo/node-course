6eca2b644891a498bdbd98bd338f683373ae5e9d
Vlad Vashchuk <vashchukvlad17@gmail.com>
2022-11-30 22:10:02 +0200

    views init

--- src/app.js
+++ src/app.js
@@ -1,14 +1,23 @@
-const express = require("express");
+const path = require('path');
+
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const rootDir = require('./util/path');
+const adminRoutes = require('./routes/admin-routes');
 
 const app = express();
 
-app.use((req, res, next) => {
-    console.log('first middleware');
-    next();
-});
-app.use((req, res, next) => {
-    console.log('second middleware');
-    res.json({message: 'OK'})
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, '../', 'public')));
+
+app.use('/admin', adminRoutes);
+app.get('/', (req, res, next) => {
+  res.sendFile(path.join(rootDir, 'views', 'shop.html'));
 });
 
-app.listen(3000, () => console.log("Server listen 3000"));
+app.use((req, res, next) => {
+  res.status(404).sendFile(path.join(rootDir, 'views', '404.html'));
+});
+
+app.listen(3000, () => console.log('Server listen 3000'));
